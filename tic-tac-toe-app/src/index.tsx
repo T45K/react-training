e747493458d20f79602094fc349defb0e165dce0
T45K <tasktas9@gmail.com>
2023-07-05 09:02:43 +0900

    とりあえず動く

--- tic-tac-toe-app/src/index.tsx
+++ tic-tac-toe-app/src/index.tsx
@@ -1,48 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
+const Square = ({ value, onSquareClick }: SquareProps) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
 
-class Board extends React.Component {
-  renderSquare(_: number) {
-    return <Square />;
-  }
+type SquareProps = {
+  value: string;
+  onSquareClick: () => void;
+};
 
-  render() {
-    const status = 'Next player: X';
+const getNextUser = (xIsNet: boolean): string => {
+  return xIsNet ? 'X' : 'O';
+};
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
+  const handleClick = (i: number) => {
+    if (squares[i] !== '' || linesUp(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = getNextUser(xIsNext);
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (i: number) => {
+    return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />;
+  };
+
+  const status = linesUp(squares) ? 'Winner: ' + getNextUser(!xIsNext) : 'Next player: ' + getNextUser(xIsNext);
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const linesUp = (squares: string[]): boolean => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  const hasSameString = (indexes: number[]): boolean => {
+    const base = squares[indexes[0]];
+    return indexes.map((i) => squares[i]).every((it) => it === base);
+  };
+  return lines.some((line) => squares[line[0]] !== '' && hasSameString(line));
+};
 
 class Game extends React.Component {
   render() {
@@ -62,11 +98,7 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
